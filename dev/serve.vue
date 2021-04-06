7c12c66510f2f27eb1e7ddce2e7a2d4ac46ec267
aamustapha <abdulhakeemmustapha@gmail.com>
2021-04-06 16:43:40 +0100

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { ComponentsSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  ComponentsSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <components-sample />
+  </div>
+</template>
