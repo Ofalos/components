a8250d46f5eb0d9769f2604160f5db445c13210e
aamustapha <abdulhakeemmustapha@gmail.com>
2021-04-07 20:15:21 +0100

    Add dropdown to exports

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -4,5 +4,7 @@ import OfalosRadio from "./OfalosRadio"
 import OfalosSelect from "./OfalosSelect"
 import OfalosSelectOption from "./OfalosSelectOption"
 import OfalosButton from "./OfalosButton"
+import OfalosDropdown from "./OfalosDropdown"
+import OfalosDropdownItem from "./OfalosDropdownItem"
 
-export { OfalosInput, OfalosCheckbox, OfalosRadio, OfalosSelect, OfalosSelectOption, OfalosButton }
+export { OfalosInput, OfalosCheckbox, OfalosRadio, OfalosSelect, OfalosSelectOption, OfalosButton, OfalosDropdown, OfalosDropdownItem}
