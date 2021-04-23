4bd9e7384310600891deeb5d6df8a095bbc1cb1f
Ibrahim Abdullahi Aliyu <Ibrahiimaa05@gmail.com>
2021-04-23 23:47:14 +0100

    Adds computed that gets value of component

--- src/lib-components/form.js
+++ src/lib-components/form.js
@@ -1,35 +1,45 @@
 export default {
-
-    props: {
-        modelValue: [String, Number],
-        label: String
+  props: {
+    modelValue: [String, Number],
+    label: String
+  },
+  computed: {
+    val() {
+      if (this.value) {
+        return this.value;
+      } else if (this.label) {
+        return this.label;
+      } else {
+        return this.$slots.default()[0].children;
+      }
     },
-    computed: {
-        id () {
-            return this.label ? this.label.toLowerCase().replace(/[\W_]+/g,"_") : Math.ceil(Math.random() * 100000)
-        },
-        dataProxy: {
-            get() {
-                return this.modelValue
-            },
-            set(modelValue) {
-                this.$emit('update:modelValue', modelValue)
-            }
-        }
+    id() {
+      return this.label
+        ? this.label.toLowerCase().replace(/[\W_]+/g, "_")
+        : Math.ceil(Math.random() * 100000);
     },
-    data() {
-        return {
-            isFocussed: this.modelValue?.length,
-        }
-    },
-    methods: {
-        focused() {
-            this.isFocussed = true
-            this.$emit('focus')
-        },
-        blurred() {
-            this.isFocussed = this.modelValue?.length > 0 || false
-            this.$emit('blur')
-        }
+    dataProxy: {
+      get() {
+        return this.modelValue;
+      },
+      set(modelValue) {
+        this.$emit("update:modelValue", modelValue);
+      }
     }
-}
+  },
+  data() {
+    return {
+      isFocussed: this.modelValue?.length
+    };
+  },
+  methods: {
+    focused() {
+      this.isFocussed = true;
+      this.$emit("focus");
+    },
+    blurred() {
+      this.isFocussed = this.modelValue?.length > 0 || false;
+      this.$emit("blur");
+    }
+  }
+};
