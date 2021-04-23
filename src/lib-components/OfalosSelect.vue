78943805fe6b4c228db900d9dd53826aa1ae571b
Ibrahim Abdullahi Aliyu <Ibrahiimaa05@gmail.com>
2021-04-23 23:09:23 +0100

    Updates mutlipleValues filter to splice

--- src/lib-components/OfalosSelect.vue
+++ src/lib-components/OfalosSelect.vue
@@ -9,35 +9,7 @@
           {{ isMultiple ? computedMultiple.toString() : selectedValue || "-" }}
         </span>
       </span>
-
-      <!--
-        Select popover, show/hide based on select state.
-
-        Entering: ""
-          From: ""
-          To: ""
-        Leaving: "transition ease-in duration-100"
-          From: "opacity-100"
-          To: "opacity-0"
-      -->
       <ul :class="['form-select-options']" role="listbox" v-show="isFocussed">
-        <!--
-          Select option, manage highlight styles based on mouseenter/mouseleave and keyboard navigation.
-
-          Highlighted: "text-white bg-indigo-600", Not Highlighted: "text-gray-900"
-        -->
-        <!-- <OfalosSelectOption>
-          Ghana
-        </OfalosSelectOption>
-        <OfalosSelectOption>
-          Ivory Coast
-        </OfalosSelectOption>
-        <OfalosSelectOption>
-          Cameroon
-        </OfalosSelectOption>
-        <OfalosSelectOption value="alkjdafj" selected="true">
-          Gambia
-        </OfalosSelectOption> -->
         <div v-for="(option, i) in options" :key="i">
           <ofalos-select-option @selected="updateSelected" :value="option" />
         </div>
@@ -73,19 +45,11 @@ export default {
     }
   },
   methods: {
-    // focused() {
-    //   console.log("focused");
-    // },
-    // blurred() {
-    //   console.log("blurred");
-    // },
     updateSelected(value, e) {
       if (this.isMultiple) {
         if (this.multipleValues.includes(value)) {
           e.target.classList.remove("selected")
-          this.multipleValues = this.multipleValues.filter((item) => {
-            return item != value
-          })
+          this.multipleValues.splice(this.multipleValues.indexOf(value),1)
         }else{
           e.target.classList.add("selected")
           this.multipleValues.push(value);
